14fd7e78cbe544e50e0a9892cce9e65d62affb8a
Kai Tecson <111247289+SchadenKai@users.noreply.github.com>
2024-02-04 12:03:53 +0800

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -38,7 +38,6 @@ export default function App() {
             <Skills />
             <ProjectsPage />
             <AwardsPage />
-            <BlogPage />
             <ContactsPage />
             <Footer />
           </main>
